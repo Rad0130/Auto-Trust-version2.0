46cf667830a7cb54d7c054d091a5e1f12a4e2f5f
Your Name <your.email@example.com>
2025-08-23 00:42:38 +0600

    resolved

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-// });
-
 const api = axios.create({
   baseURL: "https://auto-trust-version2-0.onrender.com/", // <-- Change this
   withCredentials: true,
